15d5f38fc1982541dae50d1aae48b32f6a2a9e38
Andrew Philpot <andrew.philpot@gmail.com>
2015-09-23 12:11:31 -0700

    typo

--- crfprocess.sh
+++ crfprocess.sh
@@ -1,9 +1,10 @@
 /usr/lib/spark/bin/spark-submit \
 --py-files crfprocess.zip \
---files eyeColor_config.txt,eyeColor_reference_wiki.txt,hairColor_config.txt,hairColor_reference_wiki.txt,hairText_config.txt,hairTexture_reference_wiki.txt,crf_test_filter.sh,crf_test \
+--files eyeColor_config.txt,eyeColor_reference_wiki.txt,hairColor_config.txt,hairColor_reference_wiki.txt,hairTexture_config.txt,hairTexture_reference_wiki.txt,crf_test_filter.sh,crf_test \
 --master yarn-client \
 --driver-memory 8g \
---executor-memory 8g \
+--executor-memory 80G  --executor-cores 5 \
 --num-executors 20 \
   crfprocess.py \
   $@
+
