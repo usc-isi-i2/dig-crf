df25551d26c8c3ab1bfa6a6f4d0178b5073a374f
Andrew Philpot <andrew.philpot@gmail.com>
2015-10-05 09:10:21 -0700

    add files for hairLength dump GC information to worker logs

--- crfprocess.sh
+++ crfprocess.sh
@@ -1,10 +1,11 @@
 /usr/lib/spark/bin/spark-submit \
 --py-files crfprocess.zip \
---files eyeColor_config.txt,eyeColor_reference_wiki.txt,hairColor_config.txt,hairColor_reference_wiki.txt,hairTexture_config.txt,hairTexture_reference_wiki.txt,crf_test_filter.sh,crf_test \
+--files eyeColor_config.txt,eyeColor_reference_wiki.txt,hairColor_config.txt,hairColor_reference_wiki.txt,hairTexture_config.txt,hairTexture_reference_wiki.txt,hairLength_config.txt,hairLength_reference_wiki.txt,crf_test_filter.sh,crf_test \
 --master yarn-client \
 --driver-memory 8g \
 --executor-memory 80G  --executor-cores 5 \
 --num-executors 20 \
+--conf "spark.executor.extraJavaOptions=-verbose:gc -XX:+PrintGCDetails -XX:+PrintGCTimeStamps" \
   crfprocess.py \
   $@
 
