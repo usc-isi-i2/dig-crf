169fb250b5b1dc37b7ea3008889e80c61eb40b44
Andrew Philpot <andrew.philpot@gmail.com>
2015-12-09 19:46:56 -0800

    conditionalize for local or cloud

--- crfprocess.sh
+++ crfprocess.sh
@@ -1,5 +1,18 @@
-/usr/lib/spark/bin/spark-submit \
---py-files crfprocess.zip \
+#!/bin/sh
+
+if [[ "${HOSTNAME}" == *avatar* ]]
+then
+    CFG=data/config
+    ZIP=data/zip
+    SPARKSUBMIT=/opt/spark/bin/spark-submit
+    $SPARKSUBMIT --py-files $ZIP/crfprocess.zip \
+--files $CFG/eyeColor_config.txt,$CFG/eyeColor_reference_wiki.txt,$CFG/hairColor_config.txt,$CFG/hairColor_reference_wiki.txt,$CFG/hairTexture_config.txt,$CFG/hairTexture_reference_wiki.txt,$CFG/hairLength_config.txt,$CFG/hairLength_reference_wiki.txt \
+--conf "spark.executor.extraJavaOptions=-verbose:gc -XX:+PrintGCDetails -XX:+PrintGCTimeStamps" \
+  crfprocess.py \
+  $@
+else
+    SPARKSUBMIT=/usr/lib/spark/bin/spark-submit
+    $SPARKSUBMIT --py-files crfprocess.zip \
 --files eyeColor_config.txt,eyeColor_reference_wiki.txt,hairColor_config.txt,hairColor_reference_wiki.txt,hairTexture_config.txt,hairTexture_reference_wiki.txt,hairLength_config.txt,hairLength_reference_wiki.txt,crf_test_filter.sh,crf_test \
 --master yarn-client \
 --driver-memory 8g \
@@ -8,4 +21,6 @@
 --conf "spark.executor.extraJavaOptions=-verbose:gc -XX:+PrintGCDetails -XX:+PrintGCTimeStamps" \
   crfprocess.py \
   $@
+fi
+
 
