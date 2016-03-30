c9d6ef0e6b2c5601acd9c2b749f69479fccf6b6c
Craig Milo Rogers <rogers@isi.edu>
2016-03-30 13:18:07 -0700

    Additional comments describing how the filter works with mapPartitions.

--- applyCrfKjSpark.py
+++ applyCrfKjSpark.py
@@ -17,14 +17,28 @@ import applyCrfKj
 
 def applyCrfKjSpark(inputPath, outputPath, numPartitions, featureListFilePath, modelFilePath, debug, statistics):
     def doCrf(source): 
+        """This is a filter for mapPartitions.  It takes an iterator as a source and
+returns an iterator (more precisely, a generator).  It maintains an iterator
+strategy internally, and avoids re-buffering the input and output.
+
+        """
+        # Pass the source iterator to the code t reads keyed JSON Lines
+        # data and builds sentence objects. The result, 'sentences', is also
+        # an iterator.
         sentences = crfs.CrfSentencesFromKeyedJsonLinesSource(source)
+        # Return a generator that takes the sentence iterator and produces
+        # tagged phrases in keyed JSON Lines format.  The regurned generator
+        # is initialized with featureLiesFilePath, modelFilePath, etc.
         return applyCrfKj.applyCrfKj(sentences, featureListFilePath, modelFilePath, debug, statistics)
 
-    print "Starting."
+    if debug:
+        print "Starting applyCrfKjSpark."
     sc = SparkContext()
     inputSource = sc.textFile(inputPath, numPartitions)
     results = inputSource.mapPartitions(doCrf)
     results.saveAsTextFile(outputPath)
+    if debug:
+        print "Ending applyCrfKjSpark."
 
 def main(argv=None):
     '''this is called if run from command line'''
