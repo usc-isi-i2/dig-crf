fda70bfff7ec9d25f7c08c92d570dfc52754734c
Craig Milo Rogers <rogers@isi.edu>
2016-04-12 10:07:44 -0700

    Fix typo in description scting.

--- applyCrfPjSparkTest.py
+++ applyCrfPjSparkTest.py
@@ -16,7 +16,7 @@ import applyCrf
 def main(argv=None):
     '''this is called if run from command line'''
     parser = argparse.ArgumentParser()
-    parser.add_argument('-d','--debug', help="Optionallly give debugging feedback.", required=False, action='store_true')
+    parser.add_argument('-d','--debug', help="Optionally give debugging feedback.", required=False, action='store_true')
     parser.add_argument('--download', help="Optionally ask Spark to download the feature list and model files to the clients.", required=False, action='store_true')
     parser.add_argument('-f','--featlist', help="Required input file with features to be extracted, one feature entry per line.", required=True)
     parser.add_argument('-i','--input', help="Required input file with Web scraping sentences in keyed JSON Lines format.", required=True)
