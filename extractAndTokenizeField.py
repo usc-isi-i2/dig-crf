7b47a5b135b54c814eedc4c181d6eedee4a43e88
Craig Milo Rogers <rogers@isi.edu>
2016-06-01 17:56:47 -0700

    Extract new RDD keys.

--- extractAndTokenizeField.py
+++ extractAndTokenizeField.py
@@ -1,8 +1,11 @@
-"""Given a SEQ(Text, Text) input file to use as a pair RDD (key1,
-valu1), where the SEQ value field (value1) is supposed to be a (key2,
-value2) dictionary in JSON, extract all dictionary values (value2) for
-a specific key (key2).  Return a a new pair RDD of (key1, value2).
-Print the count of records extracted."""
+"""Given a SEQ(Text, Text) input file to use as a pair RDD (key1, value1),
+where the SEQ value field (value1) is supposed to be a (key2, value2)
+dictionary in JSON, extract all dictionary values (value2) for a specific
+value key (key2).  Optionally, select an additional value (newRddKey) from each
+dictionary to use as a replacement for key1 in the output.  Return a new pair
+RDD of (key1, value2) (or (newKey1, value2)).  Print the count of records extracted.
+
+"""
 
 import argparse
 import json
@@ -16,8 +19,9 @@ def main(argv=None):
     parser = argparse.ArgumentParser()
     parser.add_argument('-c','--count', help="Optionally report a count of records extracted.", required=False, action='store_true')
     parser.add_argument('--cache', help="Optionally cache the RDD in memory.", required=False, action='store_true')
+    parser.add_argument('-k','--key', help="The key for the value being extracted.", required=True)
+    parser.add_argument('-K','--newRddKeyKey', help="The key for the value to use as the new RDD key.", required=False)
     parser.add_argument('-i','--input', help="Required Seq input file on cluster.", required=True)
-    parser.add_argument('-k','--key', help="Required extraction key.", required=True)
     parser.add_argument('-n','--notokenize', help="Optionally do not tokenize.", required=False, action='store_true')
     parser.add_argument('-o','--output', help="Optionally save in an output file.", required=False)
     parser.add_argument('--outputSeq', help="Optionally use a SEQ file for output.", required=False, action='store_true')
@@ -29,6 +33,7 @@ def main(argv=None):
 
     extractionKey = args.key
     pruning = args.prune
+    newRddKeyKey = args.newRddKeyKey
 
     tok = crft.CrfTokenizer()
     tok.setGroupPunctuation(True)
@@ -70,15 +75,39 @@ def main(argv=None):
             else:
                 return iter([])
 
+    def extractNewRddKey(value, oldKey):
+        try:
+            d = json.loads(value)
+            if newRddKeyKey in d:
+                return iter([d[newRddKeyKey]])
+            else:
+                return iter(oldKey)
+
+        except:
+            # TODO: optionally count these failures or die
+            return iter(oldKey)
+
     sc = SparkContext()
     data = sc.sequenceFile(args.input, "org.apache.hadoop.io.Text", "org.apache.hadoop.io.Text")
     if args.take > 0:
         data = sc.parallelize(data.take(args.take))
 
     if args.notokenize:
+        print "========================================"
+        print "Extracting string values"
+        print "========================================"
         extractedValuePairs = data.flatMapValues(extractStringValues)
     else:
+        print "========================================"
+        print "Extracting and tokenizing"
+        print "========================================"
         extractedValuePairs = data.flatMapValues(extractTokenValues)
+
+    if newRddKeyKey != None:
+        print "========================================"
+        print "Extracting new RDD keys."
+        print "========================================"
+        extractedValuePairs = extractedValuePairs.map(lambda x: (extractNewRddKey(x[1], [x0]), x[1]))
 
     if args.repartition > 0:
         # Repartition if increasing the number of partitions.
