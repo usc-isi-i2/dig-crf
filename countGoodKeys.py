4b768a185f6d86a1c265fdd2eb73b36fa1dc614a
Craig Milo Rogers <rogers@isi.edu>
2016-06-03 02:24:28 +0000

    Report the count of keys with bad JSON.

--- countGoodKeys.py
+++ countGoodKeys.py
@@ -9,10 +9,13 @@ import sys
 from pyspark import SparkContext
 
 def getKeys(value):
+    global goodJsonRecords, badJsonRecords
     try:
         d = json.loads(value)
+        goodJsonRecords += 1
         return iter(d.keys())
     except:
+        badJsonRecords += 1
         return iter([])
 
 def main(argv=None):
@@ -23,10 +26,15 @@ def main(argv=None):
     args = parser.parse_args()
 
     sc = SparkContext()
+    global goodJsonRecords, badJsonRecords
+    goodJsonRecords = sc.accumulator(0)
+    badJsonRecords = sc.accumulator(0)
     data = sc.sequenceFile(args.input, "org.apache.hadoop.io.Text", "org.apache.hadoop.io.Text")
     keyCounts = data.values().flatMap(getKeys).countByValue()
 
     print "========================================"
+    print "goodJsonRecords = %d" % goodJsonRecords.value
+    print "badJsonRecords = %d" % badJsonRecords.value
     for k in sorted(keyCounts):
         print k, keyCounts[k]
     print "========================================"
