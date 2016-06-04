acace7825e988aa8771a4d980788e4dec0131893
Craig Milo Rogers <rogers@isi.edu>
2016-06-04 00:47:11 +0000

    Add ./ where needed.

--- process-dump-2015-10-01-2015-12-01-aman.sh
+++ process-dump-2015-10-01-2015-12-01-aman.sh
@@ -2,43 +2,43 @@
 
 # Perform preliminary examinations of the data.  These steps may
 # be omitted from a production workflow.
-countRecords-dump-2015-10-01-2015-12-01-aman.sh \
+./countRecords-dump-2015-10-01-2015-12-01-aman.sh \
 | tee countRecords-dump-2015-10-01-2015-12-01-aman.log
 
-countGoodJson-dump-2015-10-01-2015-12-01-aman.sh \
+./countGoodJson-dump-2015-10-01-2015-12-01-aman.sh \
 | tee countGoodJson-dump-2015-10-01-2015-12-01-aman.log
 
-countGoodKeys-dump-2015-10-01-2015-12-01-aman.sh \
+./countGoodKeys-dump-2015-10-01-2015-12-01-aman.sh \
 | tee countGoodKeys-dump-2015-10-01-2015-12-01-aman.log
 
-countGoodKeysByPublisher-dump-2015-10-01-2015-12-01-aman.sh \
+./countGoodKeysByPublisher-dump-2015-10-01-2015-12-01-aman.sh \
 | tee countGoodKeysByPublisher-dump-2015-10-01-2015-12-01-aman.log
 
-extractDescriptionsAndSaveSeq-dump-2015-10-01-2015-12-01-aman.sh \
+./extractDescriptionsAndSaveSeq-dump-2015-10-01-2015-12-01-aman.sh \
 | tee extractDescriptionsAndSaveSeq-dump-2015-10-01-2015-12-01-aman.log
 
 # Perform the critical analysis steps that should be part of the
 # production workflow:
-tokenizeDescriptionsAndSaveSeq-dump-2015-10-01-2015-12-01-aman.sh \
+./tokenizeDescriptionsAndSaveSeq-dump-2015-10-01-2015-12-01-aman.sh \
 | tee tokenizeDescriptionsAndSaveSeq-dump-2015-10-01-2015-12-01-aman.log
 
-applyCrfSjSparkDescMemexHairEyes-dump-2015-10-01-2015-12-01-aman.sh \
+./applyCrfSjSparkDescMemexHairEyes-dump-2015-10-01-2015-12-01-aman.sh \
 | tee applyCrfSjSparkDescMemexHairEyes-dump-2015-10-01-2015-12-01-aman.log
 
-applyCrfSjSparkDescMemexNameEthnic-dump-2015-10-01-2015-12-01-aman.sh \
+./applyCrfSjSparkDescMemexNameEthnic-dump-2015-10-01-2015-12-01-aman.sh \
 | tee applyCrfSjSparkDescMemexNameEthnic-dump-2015-10-01-2015-12-01-aman.log
 
 # Examine the output of the CRF analyses.  These steps may be omitted
 # from a production workflow.
-countGoodKeys-dump-2015-10-01-2015-12-01-aman-results-hair-eyes.sh \
+./countGoodKeys-dump-2015-10-01-2015-12-01-aman-results-hair-eyes.sh \
 | tee countGoodKeys-dump-2015-10-01-2015-12-01-aman-results-hair-eyes.log
 
-countCrfResultTokens-dump-2015-10-01-2015-12-01-aman-results-hair-eyes.sh \
+./countCrfResultTokens-dump-2015-10-01-2015-12-01-aman-results-hair-eyes.sh \
 | tee countCrfResultTokens-dump-2015-10-01-2015-12-01-aman-results-hair-eyes.log
 
-countCrfResultTokens-dump-2015-10-01-2015-12-01-aman-results-name-ethnic.sh \
+./countCrfResultTokens-dump-2015-10-01-2015-12-01-aman-results-name-ethnic.sh \
 | tee countCrfResultTokens-dump-2015-10-01-2015-12-01-aman-results-name-ethnic.log
 
-countGoodKeys-dump-2015-10-01-2015-12-01-aman-results-name-ethnic.sh \
+./countGoodKeys-dump-2015-10-01-2015-12-01-aman-results-name-ethnic.sh \
 | tee countGoodKeys-dump-2015-10-01-2015-12-01-aman-results-name-ethnic.log
 
