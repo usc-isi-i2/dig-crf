a204a3c08c0a1845211fa391310b61cce165d100
Craig Milo Rogers <rogers@isi.edu>
2016-06-14 06:07:06 -0700

    Increase the number of executors.

--- countGoodKeys-dump-2015-10-01-2015-12-01-aman-hbase.sh
+++ countGoodKeys-dump-2015-10-01-2015-12-01-aman-hbase.sh
@@ -5,7 +5,7 @@ INPUTFILE=/user/worker/hbase-dump-2015-10-01-2015-12-01-aman/hbase
 echo "Submitting the job to the Memex cluster."
 time spark-submit \
     --master 'yarn-client' \
-    --num-executors 50 \
+    --num-executors 350 \
     --driver-java-options -Dlog4j.configuration=file:quieter-log4j.properties \
     ./countGoodKeys.py \
     -- \
