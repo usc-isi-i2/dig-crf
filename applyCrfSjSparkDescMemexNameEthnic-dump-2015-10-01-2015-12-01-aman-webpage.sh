eae94f1c37d64bfd26dc2016a7260d8f94ac5cd6
Craig Milo Rogers <rogers@isi.edu>
2016-06-14 17:01:37 -0700

    Add applyCrfSpark.py to the list of required files.

--- applyCrfSjSparkDescMemexNameEthnic-dump-2015-10-01-2015-12-01-aman-webpage.sh
+++ applyCrfSjSparkDescMemexNameEthnic-dump-2015-10-01-2015-12-01-aman-webpage.sh
@@ -27,7 +27,7 @@ echo "Submitting the job to the Memex cluster."
 time spark-submit \
     --master 'yarn-client' \
     --num-executors 350 \
-    --py-files CRF++-0.58/python/dist/mecab_python-0.0.0-py2.7-linux-x86_64.egg,crf_features.py,crf_sentences.py,crf_tokenizer.py,applyCrf.py \
+    --py-files CRF++-0.58/python/dist/mecab_python-0.0.0-py2.7-linux-x86_64.egg,crf_features.py,crf_sentences.py,crf_tokenizer.py,applyCrf.py,applyCrfSpark.py \
     --conf "spark.executorEnv.PYTHON_EGG_CACHE=${PYTHON_EGG_CACHE}" \
     --driver-java-options -Dlog4j.configuration=file:quieter-log4j.properties \
     ./applyCrfSparkTest.py \
