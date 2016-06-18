5ff8aa1f8e5c279597ad8207263995c11e710bb7
Craig Milo Rogers <rogers@isi.edu>
2016-06-18 01:10:46 +0000

    Use an envar, MEMEX_MAX_EXECUTORS, to limit the number of executors requested by the script.

--- applyCrfSjSparkDescMemexNameEthnicHj-dump-2015-10-01-2015-12-01-aman-hbase-nourl.sh
+++ applyCrfSjSparkDescMemexNameEthnicHj-dump-2015-10-01-2015-12-01-aman-hbase-nourl.sh
@@ -1,6 +1,8 @@
 #! /bin/bash
 
-# This script assumes that "spark-submit" is available on $PATH.
+# This script assumes that "spark-submit" is available on $PATH.  The
+# envar MEMEX_MAX_EXECUTORS may be used to limit the number of
+# executors used in this script.
 
 MYHOME=hdfs:///user/crogers
 
@@ -9,6 +11,12 @@ FEATURES=features.name-ethnic
 MODEL=dig-name-ethnic-train.model
 HYBRID_JACCARD=hybrid_jaccard_config.json
 OUTDIR=hbase-dump-2015-10-01-2015-12-01-aman-hbase-crf-name-ethnic-hj-nourl.seq
+NUM_EXECUTORS=350
+if [ ${NUM_EXECUTORS} -gt ${MEMEX_MAX_EXECUTORS:-${NUM_EXECUTORS}} ]
+  then
+    NUM_EXECUTORS=${MEMEX_MAX_EXECUTORS}
+fi
+echo "Requesting ${NUM_EXECUTORS} executors."
 
 PYTHON_EGG_CACHE=./python-eggs
 export PYTHON_EGG_CACHE
@@ -42,7 +50,7 @@ hadoop fs -mkdir -p $PYTHON_EGG_CACHE
 echo "Submitting the job to the Memex cluster."
 time spark-submit \
     --master 'yarn-client' \
-    --num-executors 350 \
+    --num-executors ${NUM_EXECUTORS} \
     --py-files CRF++-0.58/python/dist/mecab_python-0.0.0-py2.7-linux-x86_64.egg,pythonFiles.zip \
     --conf "spark.executorEnv.PYTHON_EGG_CACHE=${PYTHON_EGG_CACHE}" \
     --driver-java-options -Dlog4j.configuration=file:quieter-log4j.properties \
