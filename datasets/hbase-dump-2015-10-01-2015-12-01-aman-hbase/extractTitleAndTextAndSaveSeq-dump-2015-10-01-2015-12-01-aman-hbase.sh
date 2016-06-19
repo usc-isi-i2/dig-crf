a7a72fbf4628de258c102d4f6a474c09966f9af2
Craig Milo Rogers <rogers@isi.edu>
2016-06-19 23:35:14 +0000

    Apparently --pyfiles is more path sensitive than I'd like.

--- datasets/hbase-dump-2015-10-01-2015-12-01-aman-hbase/extractTitleAndTextAndSaveSeq-dump-2015-10-01-2015-12-01-aman-hbase.sh
+++ datasets/hbase-dump-2015-10-01-2015-12-01-aman-hbase/extractTitleAndTextAndSaveSeq-dump-2015-10-01-2015-12-01-aman-hbase.sh
@@ -17,11 +17,18 @@ fi
 echo "Clearing the output folder: ${OUTFILE}"
 hadoop fs -rm -r -f ${OUTFILE}
 
+# Create a zip file of all the Python files.
+rm -f ${DIG_CRF_HOME}/pythonFiles.zip
+(cd ${DIG_CRF_HOME}/src/applyCrf && zip -r ${DIG_CRF_HOME}/pythonFiles.zip \
+     crf_features.py crf_sentences.py crf_tokenizer.py \
+     applyCrf.py applyCrfSpark.py \
+     hybridJaccard)
+
 echo "Submitting the job to the Memex cluster."
 time spark-submit \
     --master 'yarn-client' \
     --num-executors 350 \
-    --py-files ${DIG_CRF_HOME}/src/applyCrf/crf_tokenizer \
+    --py-files ${DIG_CRF_HOME}/pythonFiles.zip \
     --driver-java-options -Dlog4j.configuration=file:${DIG_CRF_HOME}/data/config/quieter-log4j.properties \
     ${DIG_CRF_HOME}/src/extract/extractAndTokenizeField.py \
     -- \
