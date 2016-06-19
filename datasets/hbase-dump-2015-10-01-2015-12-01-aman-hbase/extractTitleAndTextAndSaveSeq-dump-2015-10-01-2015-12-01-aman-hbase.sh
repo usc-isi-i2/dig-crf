8887ce692ce8ce605ccfa13bde5dd81e16e49dc3
Craig Milo Rogers <rogers@isi.edu>
2016-06-19 23:27:07 +0000

    Fix a path.

--- datasets/hbase-dump-2015-10-01-2015-12-01-aman-hbase/extractTitleAndTextAndSaveSeq-dump-2015-10-01-2015-12-01-aman-hbase.sh
+++ datasets/hbase-dump-2015-10-01-2015-12-01-aman-hbase/extractTitleAndTextAndSaveSeq-dump-2015-10-01-2015-12-01-aman-hbase.sh
@@ -21,7 +21,7 @@ echo "Submitting the job to the Memex cluster."
 time spark-submit \
     --master 'yarn-client' \
     --num-executors 350 \
-    --py-files ${DIG_CRF_HOME}/crf_tokenizer \
+    --py-files ${DIG_CRF_HOME}/src/applyCrf/crf_tokenizer \
     --driver-java-options -Dlog4j.configuration=file:${DIG_CRF_HOME}/data/config/quieter-log4j.properties \
     ${DIG_CRF_HOME}/src/extract/extractAndTokenizeField.py \
     -- \
