fa69487f4d4f95ea5be2e0e2374fb93033e01bbe
Craig Milo Rogers <rogers@isi.edu>
2016-06-19 18:34:00 -0700

    Enforce executor limits.

--- datasets/hbase-dump-2015-10-01-2015-12-01-aman-hbase/countGoodKeysByTarget-dump-2015-10-01-2015-12-01-aman-hbase.sh
+++ datasets/hbase-dump-2015-10-01-2015-12-01-aman-hbase/countGoodKeysByTarget-dump-2015-10-01-2015-12-01-aman-hbase.sh
@@ -1,21 +1,15 @@
 #! /bin/bash
 
-# TODO: Fix the program and this script, they don't work on this dataset.
-
 INPUTFILE=/user/worker/hbase-dump-2015-10-01-2015-12-01-aman/hbase
+NUM_EXECUTORS=350
 
-FOUND=`fgrep tun0: /proc/net/dev`
-if  [ -n "$FOUND" ] ; then
-  echo "A tunnel is present, assuming it leads to the Memex cluster."
-else
-  echo "No tunnel found, exiting"
-  exit 1
-fi
+${DIG_CRF_HOME}/checkMemexConnection.sh
+source ${DIG_CRF_HOME}/limitMemexExecutors.sh
 
 echo "Submitting the job to the Memex cluster."
 time spark-submit \
     --master 'yarn-client' \
-    --num-executors 300\
+    --num-executors ${NUM_EXECUTORS} \
     --driver-java-options -Dlog4j.configuration=file:${DIG_CRF_HOME}/data/config/quieter-log4j.properties \
     ${DIG_CRF_HOME}/src/count/countGoodKeysByTarget.py \
     -- \
