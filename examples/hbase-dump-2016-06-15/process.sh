6e16bcc38157615b87e26d28c33b568654c0239b
Craig Milo Rogers <rogers@isi.edu>
2016-06-22 17:56:10 +0000

    Use bash code blocks, and include stderr.

--- examples/hbase-dump-2016-06-15/process.sh
+++ examples/hbase-dump-2016-06-15/process.sh
@@ -1,31 +1,41 @@
 #! /bin/bash
 
-# (time countGoodKeysByTarget.sh) \
-# | tee countGoodKeysByTarget.log
+# |& is available in bash version 4.  It copies both STDERR and
+# STDOUT to a pipe, as |& does in csh.
+#
+# The advantages of including STDERR in the logs are:
+# 1) it captures error messages for later analysis
+# 2) it captures execution time reports from bash
+#
+# The disadvantage is:
+# 1) it captures the Spark completion bars.
 
-# (time tokenizeTitleAndTextAndSaveSeq.sh) \
-# | tee tokenizeTitleAndTextAndSaveSeq.log
+{ time countGoodKeysByTarget.sh; } \
+|& tee countGoodKeysByTarget.log
 
-(time ./applyCrfSjSparkDescMemexHairEyesHj.sh) \
-| tee applyCrfSjSparkDescMemexHairEyesHj.log
+{ time tokenizeTitleAndTextAndSaveSeq.sh; } \
+|& tee tokenizeTitleAndTextAndSaveSeq.log
 
-(time ./applyCrfSjSparkDescMemexNameEthnicHj.sh) \
-| tee applyCrfSjSparkDescMemexNameEthnicHj.log
+{ time ./applyCrfSjSparkDescMemexHairEyesHj.sh; } \
+|& tee applyCrfSjSparkDescMemexHairEyesHj.log
 
-(time ./countCrfResultPhrases-crf-hair-eyes-hj.sh) \
-| tee countCrfResultPhrases-crf-hair-eyes-hj.log
+{ time ./applyCrfSjSparkDescMemexNameEthnicHj.sh; } \
+|& tee applyCrfSjSparkDescMemexNameEthnicHj.log
 
-(time ./countCrfResultPhrases-crf-name-ethnic-hj.sh) \
-| tee countCrfResultPhrases-crf-name-ethnic-hj.log
+{ time ./countCrfResultPhrases-crf-hair-eyes-hj.sh; } \
+|& tee countCrfResultPhrases-crf-hair-eyes-hj.log
 
-(time ./countCrfResultPhrasesFancy-crf-hair-eyes-hj.sh) \
-| tee countCrfResultPhrasesFancy-crf-hair-eyes-hj.log
+{ time ./countCrfResultPhrases-crf-name-ethnic-hj.sh; } \
+|& tee countCrfResultPhrases-crf-name-ethnic-hj.log
 
-(time ./countCrfResultPhrasesFancy-crf-name-ethnic-hj.sh) \
-| tee countCrfResultPhrasesFancy-crf-name-ethnic-hj.log
+{ time ./countCrfResultPhrasesFancy-crf-hair-eyes-hj.sh; } \
+|& tee countCrfResultPhrasesFancy-crf-hair-eyes-hj.log
 
-(time ./countCrfResultTokensFancy-crf-hair-eyes-hj.sh) \
-| tee countCrfResultTokensFancy-crf-hair-eyes-hj.log
+{ time ./countCrfResultPhrasesFancy-crf-name-ethnic-hj.sh; } \
+|& tee countCrfResultPhrasesFancy-crf-name-ethnic-hj.log
 
-(time ./countCrfResultTokensFancy-crf-name-ethnic-hj.sh) \
-| tee countCrfResultTokensFancy-crf-name-ethnic-hj.log
+{ time ./countCrfResultTokensFancy-crf-hair-eyes-hj.sh; } \
+|& tee countCrfResultTokensFancy-crf-hair-eyes-hj.log
+
+{ time ./countCrfResultTokensFancy-crf-name-ethnic-hj.sh; } \
+|& tee countCrfResultTokensFancy-crf-name-ethnic-hj.log
