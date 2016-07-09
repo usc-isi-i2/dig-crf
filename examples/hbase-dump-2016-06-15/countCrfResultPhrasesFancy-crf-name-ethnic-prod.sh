0a441ea5982e1c41022e47cf9baf3da6f1843442
Craig Milo Rogers <rogers@isi.edu>
2016-07-09 02:02:44 +0000

    Some additional 'production' scripts.

---
+++ examples/hbase-dump-2016-06-15/countCrfResultPhrasesFancy-crf-name-ethnic-prod.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+NUM_EXECUTORS=50
+
+source config.sh
+source ${DIG_CRF_SCRIPT}/checkMemexConnection.sh
+source ${DIG_CRF_SCRIPT}/limitMemexExecutors.sh
+
+echo "Submitting the job to the Memex cluster."
+time spark-submit \
+    --master 'yarn-client' \
+    --num-executors ${NUM_EXECUTORS} \
+    ${DRIVER_JAVA_OPTIONS} \
+    ${DIG_CRF_COUNT}/countCrfResultPhrasesFancy.py \
+    -- \
+    --input ${PRODUCTION_NAME_ETHNIC_FILE} \
+    --excludeTags url
