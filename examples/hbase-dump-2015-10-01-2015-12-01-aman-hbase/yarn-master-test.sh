a67a209428ab0457aee3cecc8f14ec9403310433
Craig Milo Rogers <rogers@isi.edu>
2016-07-08 23:46:04 -0700

    Test yarn master mode.

---
+++ examples/hbase-dump-2015-10-01-2015-12-01-aman-hbase/yarn-master-test.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+NUM_EXECUTORS=350
+
+source config.sh
+source ${DIG_CRF_SCRIPT}/checkMemexConnection.sh
+source ${DIG_CRF_SCRIPT}/limitMemexExecutors.sh
+
+echo "Submitting the job to the Memex cluster."
+time spark-submit \
+    --master 'yarn' \
+    --deploy-mode 'cluster' \
+    --num-executors ${NUM_EXECUTORS} \
+    ${DRIVER_JAVA_OPTIONS} \
+    ${DIG_CRF_COUNT}/countGoodKeysByTarget.py \
+    -- \
+    --byUrl \
+    --input ${HDFS_INPUT_DATA_DIR}
