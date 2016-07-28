20d91bb59f785cd9058546545ed1b80cb9390990
Ubuntu <ubuntu@ip-172-31-34-86.us-west-2.compute.internal>
2016-07-28 14:02:27 +0000

    Scripts customized to the AWS system.

---
+++ examples/memex-2016-eval/countCrfResultTokens-crf-hair-eyes-hj.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+NUM_EXECUTORS=32
+
+source config.sh
+
+echo "Running the job locally."
+time spark-submit \
+    --master 'local[32]' \
+    --num-executors ${NUM_EXECUTORS} \
+    ${DRIVER_JAVA_OPTIONS} \
+    ${DIG_CRF_COUNT}/countCrfResultTokens.py \
+    --input ${WORKING_HAIR_EYES_HJ_FILE} \
+    --inputTuples \
+    --printToLog \
+    --excludeTags url
---
+++ examples/memex-2016-eval/countCrfResultTokensFancy-crf-name-ethnic.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+NUM_EXECUTORS=32
+
+source config.sh
+
+echo "Running the job locally."
+time spark-submit \
+    --master 'local[32]' \
+    --num-executors ${NUM_EXECUTORS} \
+    ${DRIVER_JAVA_OPTIONS} \
+    ${DIG_CRF_COUNT}/countCrfResultTokensFancy.py \
+    --input ${WORKING_NAME_ETHNIC_FILE} \
+    --inputTuples \
+    --excludeTags url
