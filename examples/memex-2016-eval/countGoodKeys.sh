472e19a09d1d05b38b9f9b05df07f9ffd0b80ddc
Ubuntu <ubuntu@ip-172-31-34-86.us-west-2.compute.internal>
2016-07-28 10:55:20 +0000

    Count the good keys.

---
+++ examples/memex-2016-eval/countGoodKeys.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+NUM_EXECUTORS=32
+
+source config.sh
+
+echo "Running the job locally."
+time spark-submit \
+    --master 'local[32]' \
+    --num-executors ${NUM_EXECUTORS} \
+    ${DRIVER_JAVA_OPTIONS} \
+    ${DIG_CRF_COUNT}/countGoodKeysText.py \
+    --input ${HDFS_INPUT_DATA_DIR}
---
+++ src/count/countGoodKeysText.py
@@ -0,0 +1,47 @@
+"""Given a SEQ(Text, Text) input file to use as an RDD, where the
+value field is supposed to be a dictionary in JSON, count the number
+of occurances of each unique key in the set of dictionaries.  Print
+the resulting map (key => count), sorted by key."""
+
+import argparse
+import json
+import sys
+from pyspark import SparkContext
+
+def getKeys(value):
+    global goodJsonRecords, badJsonRecords
+    try:
+        d = json.loads(value)
+        goodJsonRecords += 1
+        return iter(d.keys())
+    except:
+        badJsonRecords += 1
+        return iter([])
+
+def main(argv=None):
+    '''this is called if run from command line'''
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-i','--input', help="Required Seq input file on cluster.", required=True)
+    args = parser.parse_args()
+
+    sc = SparkContext()
+    global goodJsonRecords, badJsonRecords
+    goodJsonRecords = sc.accumulator(0)
+    badJsonRecords = sc.accumulator(0)
+    data = sc.textFile(args.input).map(lambda x: eval(x))
+    keyCounts = data.values().flatMap(getKeys).countByValue()
+    sc.stop()
+
+    print "========================================"
+    print "goodJsonRecords = %d" % goodJsonRecords.value
+    print "badJsonRecords = %d" % badJsonRecords.value
+    for k in sorted(keyCounts):
+        print k, keyCounts[k]
+    print "========================================"
+
+    sc.stop()
+
+# call main() if this is run as standalone                                                             
+if __name__ == "__main__":
+    sys.exit(main())
