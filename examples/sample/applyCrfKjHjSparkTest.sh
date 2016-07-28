71627dc090d3f0a8845c0ce8e4d93f1a6a319b61
Ubuntu <ubuntu@ip-172-31-34-86.us-west-2.compute.internal>
2016-07-28 08:10:31 +0000

    Update one of the sample local Spark files to use the Python zip file.

--- examples/sample/applyCrfKjHjSparkTest.sh
+++ examples/sample/applyCrfKjHjSparkTest.sh
@@ -4,11 +4,15 @@
 
 OUTDIR=${DIG_CRF_HOME}/data/sample/adjudicated_modeled_live_eyehair_100_03-tags-hj.spark
 
+# Build the python zip file:
+../../buildPythonFiles.sh
+
 # Dangerous!
 rm -rf ${OUTDIR}
 
 spark-submit \
     --master 'local[8]' \
+    --py-files ../../pythonFiles.zip \
     ${DIG_CRF_HOME}/src/applyCrf/applyCrfSparkTest.py --keyed \
     --input ${DIG_CRF_HOME}/data/sample/adjudicated_modeled_live_eyehair_100_03.kjsonl \
     --output ${OUTDIR} \
